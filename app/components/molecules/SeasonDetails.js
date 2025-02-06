cd1248c094acb048447c6dbad67cf9917e1c3995
ebuberoderick <ebuberoderick2@gmail.com>
2025-02-06 15:06:51 +0000

    updating app UI

--- app/components/molecules/SeasonDetails.js
+++ app/components/molecules/SeasonDetails.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import { IoArrowDown } from "react-icons/io5";
+import { IoArrowDown, IoTime } from "react-icons/io5";
 
 function SeasonDetails() {
-
     const [showEpisodes, setShowEpisodes] = useState(false)
 
     return (
@@ -20,8 +19,28 @@ function SeasonDetails() {
                     <IoArrowDown />
                 </div>
             </div>
-            <div className={`${showEpisodes ? "block" : "hidden"}`}>
-                
+            <div className={`${showEpisodes ? "block" : "hidden"} space-y-6 sm:space-y-0 md:space-y-6 lg:space-y-0 lg:*:border-t lg:last:border-b sm:*:border-t sm:last:border-b md:*:border-t-0 md:last:border-b-0 last:border-gray-800 *:border-gray-800`}>
+                {
+                    Array.from(Array(9).keys()).map((index) => (
+                        <div key={index} className="sm:flex md:block lg:flex space-y-4 sm:space-y-0 md:space-y-4 lg:space-y-0 items-center rounded-2xl sm:rounded-none md:rounded-2xl lg:rounded-none py-8 bg-gray-800/40 px-6 sm:px-0 md:px-5 lg:px-0 md:bg-gray-800/40 sm:bg-[#00000000] lg:bg-[#00000000]  justify-between gap-4">
+                            <div className='flex w-full sm:w-auto md:w-full lg:w-auto items-center gap-5'>
+                                <div className='sm:order-1 md:order-none lg:order-1 w-full flex-grow'>
+                                    <div className='sm:w-32 md:w-auto lg:w-44 w-full h-28 bg-gray-700 rounded-lg'></div>
+                                </div>
+                                <div className='text-gray-600 text-2xl font-semibold'>01</div>
+                            </div>
+                            <div className='flex-grow space-y-3'>
+                                <div className='flex items-center justify-between'>
+                                    <div className='text-white font-semibold text-lg'>Chapter Three: Holly, Jolly</div>
+                                    <div>
+                                        <div className="text-gray-300/50 bg-gray-800/30 text-sm rounded-lg px-2 border border-gray-800 font-normal flex items-center justify-center gap-1"><IoTime /> 53 mins</div>
+                                    </div>
+                                </div>
+                                <div className='text-gray-500 font-normal'>Lucas, Mike and Dustin try to talk to the girl they found in the woods. Hopper questions an anxious Joyce about an unsettling phone call.</div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
