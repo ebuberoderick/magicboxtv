900e35ee149aa1afd36712229d392cc34d2df7d9
Chukwudi Eze <chukwudieze97@gmail.com>
2025-02-18 11:12:47 +0100

    minor change

---
+++ app/middleware.js
@@ -0,0 +1,10 @@
+export async function middleware(request) {
+    const headersList = await headers();
+    const msisdn = headersList.get("msisdn");
+    console.log({ middleware: msisdn })
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: '/:path*',
+  }
--- services/authService.js
+++ services/authService.js
@@ -6,30 +6,30 @@ import {
   getErrorResponse,
 } from "./httpService";
 
-export const fetchGenresAPI = cache(() =>
+export const fetchGenresAPI = () =>
   apiWithOutAuth
     .get("/api/content/genre/")
     .then(getApiResponse)
-    .catch(getErrorResponse)
-);
-export const fetchGenresMovieAPI = cache((formdata) =>
+    .catch(getErrorResponse);
+
+export const fetchGenresMovieAPI = (formdata) =>
   apiWithOutAuth
     .get(`/api/content/contents/?genre=${formdata.slug}`)
     .then(getApiResponse)
-    .catch(getErrorResponse)
-);
-export const fetchSeriesAPI = cache(() =>
+    .catch(getErrorResponse);
+
+export const fetchSeriesAPI = () =>
   apiWithOutAuth
     .get("/api/content/contents/?category=tv-series")
     .then(getApiResponse)
-    .catch(getErrorResponse)
-);
-export const fetchAPI = cache((formdata) =>
+    .catch(getErrorResponse);
+
+export const fetchAPI = (formdata) =>
   apiWithOutAuth
     .get(`/api/content/contents/?${formdata}`)
     .then(getApiResponse)
-    .catch(getErrorResponse)
-);
+    .catch(getErrorResponse);
+
 export const fetchMovieInfoAPI = (formdata) =>
   apiWithOutAuth
     .get(`/api/content/contents/${formdata}`)
