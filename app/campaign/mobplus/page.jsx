a3bcff1b48e44405408aa5549877c69c9710f821
Chukwudi Eze <chukwudieze97@gmail.com>
2025-02-18 12:31:48 +0100

    add suspense

--- app/campaign/mobplus/page.jsx
+++ app/campaign/mobplus/page.jsx
@@ -1,13 +1,15 @@
-"use client"
-import React, { useEffect } from "react";
+"use client";
+import React, { Suspense, useEffect } from "react";
 import { postCampaign } from "../../../services/authService";
 import { redirect, useSearchParams } from "next/navigation";
 
-const MobplusPage = () => {
+const Moblus = () => {
   // const { clickid, partner, telco, pubid } = await searchParamsPromise;
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
 
-  const { clickid, partner, telco, pubid } = Object.fromEntries(searchParams.entries())
+  const { clickid, partner, telco, pubid } = Object.fromEntries(
+    searchParams.entries()
+  );
 
   console.log(`processing for ${clickid}`);
 
@@ -18,7 +20,7 @@ const MobplusPage = () => {
       telco,
       pubid,
     });
-  
+
     if (status) {
       if (data?.action === "redirect") {
         redirect(data?.url);
@@ -27,16 +29,24 @@ const MobplusPage = () => {
         redirect("/");
       }
     }
-  }
+  };
 
   useEffect(() => {
     onPost();
-  }, [])
+  }, []);
 
   return (
     <div className="p-4">
       {/* <h3 className="text-white text-center text-2xl">{data?.message}</h3> */}
     </div>
+  );
+};
+
+const MobplusPage = () => {
+  return (
+    <Suspense>
+      <Moblus />
+    </Suspense>
   );
 };
 
