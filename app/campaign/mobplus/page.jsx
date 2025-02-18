416952814baba200f19930e933e499d5be119b14
Chukwudi Eze <chukwudieze97@gmail.com>
2025-02-18 12:21:26 +0100

    move mobplus to client

--- app/campaign/mobplus/page.jsx
+++ app/campaign/mobplus/page.jsx
@@ -1,31 +1,41 @@
-import React from "react";
+"use client"
+import React, { useEffect } from "react";
 import { postCampaign } from "../../../services/authService";
-import { redirect } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
 
-const MobplusPage = async ({ searchParams: searchParamsPromise }) => {
-  const { clickid, partner, telco, pubid } = await searchParamsPromise;
+const MobplusPage = () => {
+  // const { clickid, partner, telco, pubid } = await searchParamsPromise;
+  const searchParams = useSearchParams()
+
+  const { clickid, partner, telco, pubid } = Object.fromEntries(searchParams.entries())
 
   console.log(`processing for ${clickid}`);
 
-  const { data, status } = await postCampaign("/api/ums/provider/mobplus", {
-    clickid,
-    partner,
-    telco,
-    pubid,
-  });
-
-  if (status) {
-    if (data?.action === "redirect") {
-      redirect(data?.url);
-    }
-    if (data?.action === "homepage") {
-      redirect("/");
+  const onPost = async () => {
+    const { data, status } = await postCampaign("/api/ums/provider/mobplus", {
+      clickid,
+      partner,
+      telco,
+      pubid,
+    });
+  
+    if (status) {
+      if (data?.action === "redirect") {
+        redirect(data?.url);
+      }
+      if (data?.action === "homepage") {
+        redirect("/");
+      }
     }
   }
 
+  useEffect(() => {
+    onPost();
+  }, [])
+
   return (
     <div className="p-4">
-      <h3 className="text-white text-center text-2xl">{data?.message}</h3>
+      {/* <h3 className="text-white text-center text-2xl">{data?.message}</h3> */}
     </div>
   );
 };
