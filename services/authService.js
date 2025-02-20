fd7a28ce2fe4ab3a0c3177a6ccc6371df07efd69
ebuberoderick <ebuberoderick2@gmail.com>
2025-02-20 13:11:35 +0000

    revert code

--- services/authService.js
+++ services/authService.js
@@ -6,13 +6,13 @@ import {
 
 export const fetchGenresAPI = () =>
   apiWithOutAuth
-    .get("/content/genre/")
+    .get("/api/content/genre/")
     .then(getApiResponse)
     .catch(getErrorResponse);
 
 export const fetchMovieInfoAPI = (formdata) =>
   apiWithOutAuth
-    .get(`/content/contents/${formdata}`)
+    .get(`/api/content/contents/${formdata}`)
     .then(getApiResponse)
     .catch(getErrorResponse);
 
@@ -24,7 +24,7 @@ export const getMsisdn = () =>
 
 export const getContentsAPI = (params = {}) =>
   apiWithOutAuth
-    .get(`/content/contents`, { params })
+    .get(`/api/content/contents`, { params })
     .then(getApiResponse)
     .catch(getErrorResponse);
 
